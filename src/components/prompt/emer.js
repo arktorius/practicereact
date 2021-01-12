905b37a0e57ed412d214e62c888dab3a4f565e49
unknown <lunient@gmail.com>
2021-01-12 22:10:16 +0100

    ico

--- src/components/prompt/emer.js
+++ src/components/prompt/emer.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Button,Modal} from 'react-bootstrap';
+import { Button, Modal } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.css';
 
 import imageM1 from '../assets/mermesinformatica/01.png'
@@ -8,81 +8,81 @@ import imageM3 from '../assets/mermesinformatica/03.jpg'
 import imageM4 from '../assets/mermesinformatica/04.jpg'
 
 
-class Emer extends React.Component{
-  state={
+class Emer extends React.Component {
+  state = {
     abierto: false,
   }
 
-  abrirModal=()=>{
-    this.setState({abierto: !this.state.abierto});
+  abrirModal = () => {
+    this.setState({ abierto: !this.state.abierto });
   }
 
-  render(){
+  render() {
 
-    const modalStyles={
+    const modalStyles = {
       position: "absolute",
       top: '50%',
       left: '50%',
       transform: 'translate(-50%, -50%)'
     }
-    return(
+    return (
       <>
-      <div className="principal">
-        <div className="secundario">
-      <Button  onClick={this.abrirModal} className="btn-dark">Memes</Button>
+        <div className="principal">
+          <div className="secundario">
+            <Button onClick={this.abrirModal} className="btn-dark">Memes</Button>
 
-      </div></div>
+          </div></div>
 
-      <Modal show={this.state.abierto} style={modalStyles}>
-        <Modal.Header>
-          Memes
+        <Modal show={this.state.abierto} style={modalStyles}>
+          <Modal.Header>
+            Memes
         </Modal.Header>
-        <Modal.Body >
-          
-         <img
-         className="d-block  ml-auto mr-auto"
-         width="387px"
-         onClick={this.abrirModal}
-         src={imageM1}
-         alt="kimmeme"
+          <Modal.Body >
 
-         >
-         </img>
+            <img
+              className="d-block  ml-auto mr-auto"
+              width="387px"
+              onClick={this.abrirModal}
+              src={imageM1}
+              alt="kimmeme"
 
-         <img
-         className="d-block  ml-auto mr-auto"
-         width="387px"
-         onClick={this.abrirModal}
-         src={imageM2}
-         alt="pushmeme"
+            >
+            </img>
 
-         >
-         </img>
+            <img
+              className="d-block  ml-auto mr-auto"
+              width="387px"
+              onClick={this.abrirModal}
+              src={imageM2}
+              alt="pushmeme"
 
-         <img
-         className="d-block  ml-auto mr-auto"
-         width="387px"
-         src={imageM3}
-         onClick={this.abrirModal}
-         alt="homermeme"
+            >
+            </img>
 
-         >
-         </img>
-         <img
-         className="d-block  ml-auto mr-auto"
-         width="387px"
-         src={imageM4}
-         onClick={this.abrirModal}
-         alt="meme"
+            <img
+              className="d-block  ml-auto mr-auto"
+              width="387px"
+              src={imageM3}
+              onClick={this.abrirModal}
+              alt="homermeme"
 
-         >
-         </img>
-        </Modal.Body>
+            >
+            </img>
+            <img
+              className="d-block  ml-auto mr-auto"
+              width="387px"
+              src={imageM4}
+              onClick={this.abrirModal}
+              alt="meme"
 
-        <Modal.Footer>
+            >
+            </img>
+          </Modal.Body>
+
+          <Modal.Footer>
             <Button className="btn-light" onClick={this.abrirModal}>Cerrar</Button>
-        </Modal.Footer>
-      </Modal>
+          </Modal.Footer>
+        </Modal>
       </>
     )
   }
